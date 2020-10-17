0cbfcb3dcc10c0858a7f4033e0d88a9a204337b8
SeanDaniels <sdaniel5@ncsu.edu>
2020-10-17 16:47:09 -0400

    Added print statements to spinlock functions

--- xinu/system/spinlock.c
+++ xinu/system/spinlock.c
@@ -1,7 +1,10 @@
 #include "../include/xinu.h"
-
+#define DBG
+/* LOCKED = 1  */
+/* UNLOCKED = 0 */
 syscall sl_initlock(sl_lock_t *l){
     static uint32 available_locks = NPSINLOCKS;
+    kprintf("Initializing lock\n");
     if(!available_locks){
         return SYSERR;
     }
@@ -11,15 +14,30 @@ syscall sl_initlock(sl_lock_t *l){
     return OK;
 }
 syscall sl_lock(sl_lock_t *l){
+    #ifdef DBG
+    kprintf("\nThread %d attempting critical section\n", currpid);
+    #endif
     while(test_and_set(&(l->lock_value), 1)==1);
+    #ifdef DBG
+    kprintf("Lock retrieved, setting owner to thread %d\n", currpid);
+    #endif
     l->lock_owner = currpid;
     return OK;
 }
 syscall sl_unlock(sl_lock_t *l){
+#ifdef DBG
+    kprintf("Thread %d attempting to release lock\n", currpid);
+#endif // DBG
     if(l->lock_owner!=currpid){
-       return SYSERR; 
+#ifdef DBG
+        kprintf("**Error (thread %d not lock owner)**\n", currpid);
+#endif // MACRO
+       return SYSERR;
     }
     l->lock_value = UNLOCKED;
-    l->lock_owner = NOOWNER;
+    l->lock_owner = (pid32) 0;
+#ifdef DBG
+    kprintf("Thread %d released lock\n");
+#endif
     return OK;
 }
