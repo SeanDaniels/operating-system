2e8381540812f6fad05dc6be37bf9841ba25d022
SeanDaniels <sdaniel5@ncsu.edu>
2020-10-23 16:38:46 -0400

    Updated code

--- xinu/p3-testcases/test_timing.c
+++ xinu/p3-testcases/test_timing.c
@@ -1,77 +1,77 @@
 /*  main.c  - main */
 
-#include <xinu.h>
+#include "../include/xinu.h"
 
-uint32 get_timestamp(){
-	return ctr1000;
+uint32 get_timestamp() { return ctr1000; }
+
+void run_for_ms(uint32 time) {
+  uint32 start = proctab[currpid].runtime;
+  while (proctab[currpid].runtime - start < time)
+    ;
 }
 
-void run_for_ms(uint32 time){
-	uint32 start = proctab[currpid].runtime;
-	while (proctab[currpid].runtime-start < time);
+process p_spinlock(sl_lock_t *l) {
+  uint32 i;
+  for (i = 0; i < 5; i++) {
+    sl_lock(l);
+    run_for_ms(1000);
+    sl_unlock(l);
+  }
+  return OK;
 }
 
-process p_spinlock(sl_lock_t *l){
-	uint32 i;
-	for (i=0; i<5; i++){
-		sl_lock(l);
-		run_for_ms(1000);
-		sl_unlock(l);		
-	}
-	return OK;
+process p_lock(lock_t *l) {
+  uint32 i;
+  for (i = 0; i < 5; i++) {
+    lock(l);
+    run_for_ms(1000);
+    unlock(l);
+  }
+  return OK;
 }
-	
-process p_lock(lock_t *l){
-	uint32 i;
-	for (i=0; i<5; i++){
-		lock(l);
-		run_for_ms(1000);
-		unlock(l);		
-	}
-	return OK;
-}
-	
-process	main(void)
-{
 
-	sl_lock_t	mutex_sl;  		
-	lock_t 		mutex;  		
-	pid32		pid1, pid2;
-	uint32 		timestamp;
+process main(void) {
 
-	kprintf("\n\n=========== TEST 1: spinlock & 2 threads  ===================\n\n");
- 	sl_initlock(&mutex_sl); 
-	
-	pid1 = create((void *)p_spinlock, INITSTK, 1,"nthreads", 1, &mutex_sl);
-	pid2 = create((void *)p_spinlock, INITSTK, 1,"nthreads", 1, &mutex_sl);
+  sl_lock_t mutex_sl;
+  lock_t mutex;
+  pid32 pid1, pid2;
+  uint32 timestamp;
 
-	timestamp = get_timestamp();
-	
-	resume(pid1);
-	sleepms(500);
-	resume(pid2);		
+  kprintf(
+      "\n\n=========== TEST 1: spinlock & 2 threads  ===================\n\n");
+  sl_initlock(&mutex_sl);
 
-	receive();
-	receive();
+  pid1 = create((void *)p_spinlock, INITSTK, 1, "nthreads", 1, &mutex_sl);
+  pid2 = create((void *)p_spinlock, INITSTK, 1, "nthreads", 1, &mutex_sl);
 
-        kprintf("Time = %d ms\n", get_timestamp()-timestamp);
-        
-	kprintf("\n\n=========== TEST 2: lock w/sleep & 2 threads  ===============\n\n");
-	initlock(&mutex);
+  timestamp = get_timestamp();
 
-        pid1 = create((void *)p_lock, INITSTK, 1,"nthreads", 1, &mutex);
-        pid2 = create((void *)p_lock, INITSTK, 1,"nthreads", 1, &mutex);
+  resume(pid1);
+  sleepms(500);
+  resume(pid2);
 
-        timestamp = get_timestamp();
+  receive();
+  receive();
 
-        resume(pid1);
-        sleepms(500);
-        resume(pid2);
+  kprintf("Time = %d ms\n", get_timestamp() - timestamp);
 
-        receive();
-        receive();
+  kprintf(
+      "\n\n=========== TEST 2: lock w/sleep & 2 threads  ===============\n\n");
+  initlock(&mutex);
 
-        kprintf("Time = %d ms\n", get_timestamp()-timestamp);
+  pid1 = create((void *)p_lock, INITSTK, 1, "nthreads", 1, &mutex);
+  pid2 = create((void *)p_lock, INITSTK, 1, "nthreads", 1, &mutex);
 
-	return OK;
+  timestamp = get_timestamp();
+
+  resume(pid1);
+  sleepms(500);
+  resume(pid2);
+
+  receive();
+  receive();
+
+  kprintf("Time = %d ms\n", get_timestamp() - timestamp);
+
+  return OK;
 }
--- xinu/system/active_lock.c
+++ xinu/system/active_lock.c
@@ -29,8 +29,10 @@ syscall al_lock(al_lock_t *l) {
     ;
   // no process holds lock
   if (l->flag == 0) {
-    l->flag = 1;
-    l->guard = 0;
+
+    l->flag = 1; /* Set flag to 1 (lock is held) */
+    l->guard =
+        0; /* Set guard to 0, it is now safe for other threads to attempt*/
     l->owner = currpid;
   }
   // some process already holds lock
@@ -43,11 +45,15 @@ syscall al_lock(al_lock_t *l) {
   return OK;
 }
 
+/* At unlock, if no threads waiting on lock, set lock owner to -1
+ * If there are threads waiting on the lock, let the the lock function update
+ * lock owner */
 syscall al_unlock(al_lock_t *l) {
   while (test_and_set(&(l->guard), 1) == 1)
     ;
   if (isempty(l->queue)) {
     l->flag = 0;
+    l->owner = -1;
   } else {
     unpark(dequeue(l->queue));
   }
@@ -62,6 +68,8 @@ syscall al_park(al_lock_t *l) {
   prptr = &proctab[currpid];
   if (prptr->park_flag == 1) {
     prptr->prstate = PR_WAIT;
+    /* Indicate in the proccess table that this process is waiting on this lock
+     */
     prptr->prlock = l->lock_number;
     resched();
     prptr->park_flag = 0;
@@ -71,6 +79,17 @@ syscall al_park(al_lock_t *l) {
 #ifdef LOCK_DBG
   kprintf("Set park has been disabled by some other thread\n");
 #endif
+  return OK;
+}
+
+syscall al_unpark(pid32 thread) {
+  int mask;
+  mask = disable();
+  struct procent *prptr = &proctab[thread];
+  prptr->park_flag = 0;
+  prptr->prlock = -1;
+  ready(thread);
+  restore(mask);
   return OK;
 }
 
