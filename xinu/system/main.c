92c7fa1322dd24b90a115f7015050cc9ac5d30c5
SeanDaniels <sdaniel5@ncsu.edu>
2020-10-23 20:35:36 -0400

    Updated main.c
    Renamed deadlock_test to main. My testcases are now in main.c
    Old main.c renamed back to test_time.c, put in temp dir

--- xinu/system/main.c
+++ xinu/system/main.c
@@ -1,85 +1,59 @@
-/*  main.c  - main */
-
 #include "../include/xinu.h"
+#include <stdio.h>
+#define DBG
+#define DEADLOCK
+void sync_printf(char *fmt, ...) {
+  intmask mask = disable();
+  void *arg = __builtin_apply_args();
+  __builtin_apply((void *)kprintf, arg, 100);
+  restore(mask);
+}
 
-// uint32 get_timestamp() { return ctr1000; }
-
-//#define DOTEST
-
-#ifdef DOTEST
 void run_for_ms(uint32 time) {
   uint32 start = proctab[currpid].runtime;
   while (proctab[currpid].runtime - start < time)
     ;
 }
-#endif
+/* pass shared variable, lock */
 
-process p_spinlock(sl_lock_t *l) {
+process lock_routine_a(al_lock_t *l_a, al_lock_t *l_b) {
   uint32 i;
-  for (i = 0; i < 5; i++) {
-    sl_lock(l);
+  for (i = 0; i < 2; i++) {
+    al_lock(l_a);
     run_for_ms(1000);
-    sl_unlock(l);
+    al_lock(l_b);
+    run_for_ms(1000);
+    al_unlock(l_b);
+    al_unlock(l_a);
   }
   return OK;
 }
 
-process p_lock(lock_t *l) {
+process lock_routine_b(al_lock_t *l_a, al_lock_t *l_b) {
   uint32 i;
-  for (i = 0; i < 5; i++) {
-    lock(l);
+  for (i = 0; i < 2; i++) {
+    al_lock(l_b);
     run_for_ms(1000);
-    unlock(l);
+    al_lock(l_a);
+    run_for_ms(1000);
+    al_unlock(l_a);
+    al_unlock(l_b);
   }
   return OK;
 }
 
-#ifdef DOTEST
 process main(void) {
-
-  sl_lock_t mutex_sl;
-  lock_t mutex;
   pid32 pid1, pid2;
-  uint32 timestamp;
-
-  kprintf(
-      "\n\n=========== TEST 1: spinlock & 2 threads  ===================\n\n");
-  sl_initlock(&mutex_sl);
-
-  pid1 = create((void *)p_spinlock, INITSTK, 1, "nthreads", 1, &mutex_sl);
-  pid2 = create((void *)p_spinlock, INITSTK, 1, "nthreads", 1, &mutex_sl);
-
-  timestamp = get_timestamp();
-
+  al_lock_t l_a, l_b;
+  al_init_lock(&l_a);
+  al_init_lock(&l_b);
+  pid1 = create((void *)lock_routine_a, INITSTK, 1, "lr_a", 2, &l_a, &l_b);
+  pid2 = create((void *)lock_routine_b, INITSTK, 1, "lr_b", 2, &l_a, &l_b);
   resume(pid1);
   sleepms(500);
   resume(pid2);
-
   receive();
   receive();
-
-  kprintf("Time = %d ms\n", get_timestamp() - timestamp);
-
-  kprintf(
-      "\n\n=========== TEST 2: lock w/sleep & 2 threads  ===============\n\n");
-  initlock(&mutex);
-
-  pid1 = create((void *)p_lock, INITSTK, 1, "nthreads", 1, &mutex);
-  pid2 = create((void *)p_lock, INITSTK, 1, "nthreads", 1, &mutex);
-
-  timestamp = get_timestamp();
-
-  resume(pid1);
-  sleepms(500);
-  resume(pid2);
-
-  receive();
-  receive();
-
-  kprintf("Time = %d ms\n", get_timestamp() - timestamp);
-
-  kprintf("Done\n");
-
+  sync_printf("\nDone\n");
   return OK;
 }
-#endif
