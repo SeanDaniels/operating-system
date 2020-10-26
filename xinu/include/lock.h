0e1da9f6a08fa43ae9ae8a55359705b38b4b6ba7
SeanDaniels <sdaniel5@ncsu.edu>
2020-10-26 11:57:34 -0400

    Updated function definitions, macro definition
    Changed NOOWNER macro to 500 instead of 0. A lock will never
    be owned by process 500 because there will never be 500 processes

--- xinu/include/lock.h
+++ xinu/include/lock.h
@@ -5,7 +5,7 @@
 
 #define LOCKED 1
 #define UNLOCKED 0
-#define NOOWNER 0
+#define NOOWNER 500
 
 typedef struct {
   uint32 lock_value;
@@ -29,3 +29,8 @@ typedef struct {
 syscall setPark();
 syscall park();
 syscall unpark(pid32 thread);
+syscall al_park(al_lock_t *l);
+int32 get_owner(al_lock_t *l, pid32 rootProcess);
+bool8 al_try_lock(al_lock_t *l);
+void print_deadlock();
+extern bool8 deadlock_chain[NPROC];
