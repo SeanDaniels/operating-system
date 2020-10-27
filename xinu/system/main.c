27ffa806a8c02aca788f308aca7bfd6d56c90a49
SeanDaniels <sdaniel5@ncsu.edu>
2020-10-27 13:21:43 -0400

    Updated main.c
    This will be used as deadlock_main.c. Added conditional compilation
    directives for building with trylock function

--- xinu/system/main.c
+++ xinu/system/main.c
@@ -1,7 +1,8 @@
 #include "../include/xinu.h"
 #include <stdio.h>
-#define DBG
+//#define DBG
 #define DEADLOCK
+//#define TRYLOCK
 void sync_printf(char *fmt, ...) {
   intmask mask = disable();
   void *arg = __builtin_apply_args();
@@ -54,11 +55,54 @@ process lock_routine_b(al_lock_t *l_a, al_lock_t *l_b) {
   return OK;
 }
 
+process lock_routine_c(al_lock_t *l_a, al_lock_t *l_b) {
+  uint32 i;
+  bool8 temp;
+  for (i = 0; i < 2; i++) {
+    temp = al_trylock(l_a);
+#ifdef DBG
+    kprintf("Process %d has aquired lock %X\n", currpid, l_a->lock_number);
+#endif
+    run_for_ms(1000);
+    al_trylock(l_b);
+#ifdef DBG
+    kprintf("Process %d has aquired lock %X\n", currpid, l_b->lock_number);
+#endif
+    run_for_ms(1000);
+    al_unlock(l_b);
+    al_unlock(l_a);
+  }
+  return OK;
+}
+
+process lock_routine_d(al_lock_t *l_a, al_lock_t *l_b) {
+  uint32 i;
+  bool8 temp;
+  for (i = 0; i < 2; i++) {
+    al_trylock(l_b);
+#ifdef DBG
+    kprintf("Process %d has aquired lock %X\n", currpid, l_b->lock_number);
+#endif
+    run_for_ms(1000);
+    al_trylock(l_a);
+#ifdef DBG
+    kprintf("Process %d has aquired lock %X\n", currpid, l_a->lock_number);
+#endif
+    run_for_ms(1000);
+    al_unlock(l_a);
+    al_unlock(l_b);
+  }
+  return OK;
+}
+
 process main(void) {
-  pid32 pid1, pid2, pid3, pid4;
-  al_lock_t l_a, l_b, l_c, l_d;
+  pid32 pid1, pid2;
+  al_lock_t l_a, l_b;
   al_initlock(&l_a);
   al_initlock(&l_b);
+#ifdef DEADLOCK
+  pid32 pid3, pid4;
+  al_lock_t l_c, l_d;
   al_initlock(&l_c);
   al_initlock(&l_d);
   pid1 = create((void *)lock_routine_a, INITSTK, 2, "lr_a", 2, &l_a, &l_b);
@@ -76,6 +120,16 @@ process main(void) {
   receive();
   receive();
   receive();
+#endif
+#ifdef TRYLOCK
+  pid1 = create((void *)lock_routine_c, INITSTK, 2, "lr_c", 2, &l_a, &l_b);
+  pid2 = create((void *)lock_routine_d, INITSTK, 2, "lr_d", 2, &l_a, &l_b);
+  resume(pid1);
+  sleepms(500);
+  resume(pid2);
+  receive();
+  receive();
+#endif
   sync_printf("\nDone\n");
   return OK;
 }
